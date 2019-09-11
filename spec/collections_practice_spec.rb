0784a94d95b32814fd57b7faad7b75c807bd2ba3
KG ZZ <czheng5h88@gmail.com>
2019-09-11 14:25:37 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -29,6 +29,12 @@ describe 'collections practice' do
       expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
     end
   end
+  
+  describe '#swap_elements_from_to' do
+    it 'swap the index and dstination index elements of an array' do
+      expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
+    end
+  end
 
   # Question 5
   describe '#reverse_array' do
